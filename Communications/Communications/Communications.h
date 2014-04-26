e72327c159f7b2bf721313fb6fcb5ba340f050b8
John Ahrens <john@john-ahrens.com>
2014-04-26 15:28:08 -0700

    First build of ASN.1

---
+++ Communications/Communications/Communications.h
@@ -0,0 +1,13 @@
+//
+//  Communications.h
+//  Communications
+//
+//  Created by John Ahrens on 4/26/14.
+//  Copyright (c) 2014 John Ahrens. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Communications : NSObject
+
+@end
---
+++ Communications/Communications/Communications.m
@@ -0,0 +1,13 @@
+//
+//  Communications.m
+//  Communications
+//
+//  Created by John Ahrens on 4/26/14.
+//  Copyright (c) 2014 John Ahrens. All rights reserved.
+//
+
+#import "Communications.h"
+
+@implementation Communications
+
+@end
