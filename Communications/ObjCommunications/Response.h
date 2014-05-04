681fe9bb5571ef448329b69652eb4c7ffaa1a4c4
John Ahrens <john@john-ahrens.com>
2014-05-04 14:22:41 -0700

    Location up to date and pass tests

---
+++ Communications/ObjCommunications/Response.h
@@ -0,0 +1,13 @@
+//
+//  Response.h
+//  Communications
+//
+//  Created by John Ahrens on 5/4/14.
+//  Copyright (c) 2014 John Ahrens. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Response : NSObject
+
+@end
