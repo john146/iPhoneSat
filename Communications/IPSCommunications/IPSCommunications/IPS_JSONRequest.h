2b46b859e1b2b73ad013b725efea662cfcd3b0ff
John Ahrens <john@john-ahrens.com>
2014-07-15 21:06:08 -0700

    New class.

---
+++ Communications/IPSCommunications/IPSCommunications/IPS_JSONRequest.h
@@ -0,0 +1,16 @@
+//
+//  IPS_JSONRequest.h
+//  IPSCommunications
+//
+//  Created by John Ahrens on 7/15/14.
+//  Copyright (c) 2014 John Ahrens. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "IPSRequest.h"
+
+@interface IPS_JSONRequest : IPSRequest
+
+
+@end
---
+++ Communications/IPSCommunications/IPSCommunications/IPS_JSONRequest.m
@@ -0,0 +1,15 @@
+//
+//  IPS_JSONRequest.m
+//  IPSCommunications
+//
+//  Created by John Ahrens on 7/15/14.
+//  Copyright (c) 2014 John Ahrens. All rights reserved.
+//
+
+#import "IPS_JSONRequest.h"
+
+@implementation IPS_JSONRequest
+
+
+
+@end
